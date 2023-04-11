9f49f0b0a863ef172e7a05ee21f2de71ff1ee5c9
apstav <apostolis.stav@gmail.com>
2023-04-11 20:42:52 +0300

    initial commit

---
+++ src/app/users/user-insert/user-insert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-insert',
+  templateUrl: './user-insert.component.html',
+  styleUrls: ['./user-insert.component.css']
+})
+export class UserInsertComponent {
+
+}
