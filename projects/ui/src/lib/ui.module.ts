d15ed3542376354f24b97425141abbcd03c10132
apstav <apostolis.stav@gmail.com>
2023-04-24 19:27:59 +0300

    ui module

--- projects/ui/src/lib/ui.module.ts
+++ projects/ui/src/lib/ui.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { UiComponent } from './ui.component';
 import { DropdownComponent } from './dropdown/dropdown.component';
 import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [UiComponent, DropdownComponent],
-  imports: [RouterModule],
-  exports: [UiComponent],
+  imports: [BrowserModule, RouterModule],
+  exports: [UiComponent, DropdownComponent],
 })
 export class UiModule {}
